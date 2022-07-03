644aceab8396b9a485ab248a6f2d72bfe99b2fe3
KRITTIKA0422 <98336260+KRITTIKA0422@users.noreply.github.com>
2022-07-03 14:40:56 +0530

    Add files via upload

---
+++ mongo_day2.js
@@ -0,0 +1,297 @@
+//Design database for Zen class programme for October,2020
+db.zenofoctober.insertMany([ {users:  "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh"   ,
+codekata:  "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"   ,
+attendance:{present: 20   ,absent:0   ,date:6},
+topics: "Java-Looping Concept" ,
+tasks:{name: "Write a code to rotate an array by 3 times" ,submit:17 ,notsubmit:3 },
+company_drives:{name: "CTS" ,date:7,user_appeared:  "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal"   },
+mentors:{name: "Kartik Saha" ,mentee:20}
+},
+{users:  "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity"   ,
+codekata:  "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70"   ,
+attendance:{present: 11,absent:2,date:7},
+topics: "Java-Strings" ,
+tasks:{name: "Write a code to reverse a string input" ,submit:9 ,notsubmit:2  },
+company_drives:{name: "IBM" ,date:8,user_appeared:  "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen"  },
+mentors:{name: "Sanjukta Modi" ,mentee:13}
+},
+{users:  "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder"   ,
+codekata:  "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57"   ,
+attendance:{present:12,absent: 4 ,date:8},
+topics: "Backend vs. Frontend code",
+tasks:{name: "Write the difference between frontend and backend programming" ,submit:10  ,notsubmit:2   },
+company_drives:{name: "Myntra" ,date:9,user_appeared:  "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja"   },
+mentors:{name: "Gargi Roy" ,mentee:16}
+},
+{users:  "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh"   ,
+codekata:  "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"   ,
+attendance:{present:16 ,absent:4   ,date:9},
+topics: "ReactJS" ,
+tasks:{name: "Write a code to design a movie App" ,submit:14,notsubmit: 2 },
+company_drives:{name: "Instagram" ,date:10,user_appeared:  "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar"   },
+mentors:{name: "Ramesh Bhardwaj" ,mentee:20}
+},
+{users:  "Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh"   ,
+codekata:  "50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"  ,
+attendance:{present:13 ,absent: 3  ,date:15},
+topics: "Javascript" ,
+tasks:{name: "Write a code to print prime numbers in an array" ,submit: 11,notsubmit: 2  },
+company_drives:{name: "Amazon" ,date:15,user_appeared:  "Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag"   },
+mentors:{name: "Gopal Anand" ,mentee:16}
+},
+{users:  "Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh"   ,
+codekata:  "12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"   ,
+attendance:{present:17,absent:2  ,date:16},
+topics: "MySQL" ,
+tasks:{name: "Write the queries for a library database" ,submit:16 ,notsubmit: 1},
+company_drives:{name: "Microsoft" ,date:16,user_appeared:  "Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal"   },
+mentors:{name: "Raghav Kumar" ,mentee:19}
+},
+{users:  "Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh"   ,
+codekata:  "15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"   ,
+attendance:{present:10,absent:2  ,date:17},
+topics: "HTML" ,
+tasks:{name: "Write a html code to view HELLO in webpage" ,submit:8 ,notsubmit: 2 },
+company_drives:{name: "Citrus" ,date:19,user_appeared:  "Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Ramkrishna Pal , Jessica Menon"   },
+mentors:{name: "Sam Anand" ,mentee:12}
+},
+{users:  "Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh"   ,
+codekata:  "51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"   ,
+attendance:{present:8,absent:2   ,date:18},
+topics: "Javascript-DOM" ,
+tasks:{name: "Write a code to view HELLO using DOM" ,submit:6,notsubmit:2   },
+company_drives:{name: "Amdocs" ,date:19,user_appeared:  "Tamal Saha , Tanu Roy , Jyoti Maity , Sandarva Podder , Ramkrishna Pal , Animesh Singh"   },
+mentors:{name: "Ganesh Murti" ,mentee:10}
+},
+{users:  "Rahul Patel , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh"   ,
+codekata:  "65 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"   ,
+attendance:{present:16 ,absent: 2   ,date:29},
+topics: "NodeJS-File system" ,
+tasks:{name: "Write a code to excecute the file system task" ,submit: 16 ,notsubmit:0   },
+company_drives:{name: "Facebook" ,date:23,user_appeared:  "Rahul Patel , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh"   },
+mentors:{name: "Rajendra Prasad" ,mentee:18}
+},
+{users:  "Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh"   ,
+codekata:  "55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"   ,
+attendance:{present:15,absent: 2  ,date:30},
+topics: "NodeJS-CRUD operations" ,
+tasks:{name: "Write a code to execute the CRUD operations in NodeJS" ,submit: 10,notsubmit: 5 },
+company_drives:{name: "Twitter" ,date:24,user_appeared:  "Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity"   },
+mentors:{name: "Shyam Prasad" ,mentee:17}
+}
+]);
+
+
+
+1. Find all the topics and tasks which are thought in the month of October
+ANS- db.zenofoctober.find({},{_id:0,topics:1,tasks:{name:1}}).pretty();
+OUTPUT-
+{
+    "topics" : "Java-Looping Concept",
+    "tasks" : {
+            "name" : "Write a code to rotate an array by 3 times"
+    }
+}
+{
+    "topics" : "Java-Strings",
+    "tasks" : {
+            "name" : "Write a code to reverse a string input"
+    }
+}
+{
+    "topics" : "Backend vs. Frontend code",
+    "tasks" : {
+            "name" : "Write the difference between frontend and backend programming"
+    }
+}
+{
+    "topics" : "ReactJS",
+    "tasks" : {
+            "name" : "Write a code to design a movie App"
+    }
+}
+{
+    "topics" : "Javascript",
+    "tasks" : {
+            "name" : "Write a code to print prime numbers in an array"
+    }
+}
+{
+    "topics" : "MySQL",
+    "tasks" : {
+            "name" : "Write the queries for a library database"
+    }
+}
+{
+    "topics" : "HTML",
+    "tasks" : {
+            "name" : "Write a html code to view HELLO in webpage"
+    }
+}
+{
+    "topics" : "Javascript-DOM",
+    "tasks" : {
+            "name" : "Write a code to view HELLO using DOM"
+    }
+}
+{
+    "topics" : "NodeJS-File system",
+    "tasks" : {
+            "name" : "Write a code to excecute the file system task"
+    }
+}
+{
+    "topics" : "NodeJS-CRUD operations",
+    "tasks" : {
+            "name" : "Write a code to execute the CRUD operations in NodeJS"
+    }
+}
+
+2. Find all the company drives which appeared between 15 oct-2020 and 31-oct-2020
+ANS- db.zenofoctober.find({
+    "company_drives.date": {
+      $gte: 15,
+      $lte: 31
+    }
+  },
+  {
+    _id: 0,
+    company_drives: {
+      name: 1,
+      date: 1
+    }
+  })
+  OUTPUT-
+{ "company_drives" : { "name" : "Amazon", "date" : 15 } }
+{ "company_drives" : { "name" : "Microsoft", "date" : 16 } }
+{ "company_drives" : { "name" : "Citrus", "date" : 19 } }
+{ "company_drives" : { "name" : "Amdocs", "date" : 19 } }
+{ "company_drives" : { "name" : "Facebook", "date" : 23 } }
+{ "company_drives" : { "name" : "Twitter", "date" : 24 } }
+
+3. Find all the company drives and students who are appeared for the placement.
+ANS- db.zenofoctober.find({},{_id:0,company_drives:{user_appeared:1,name:1}}).pretty();
+OUTPUT-
+{
+        "company_drives" : {
+                "name" : "CTS",
+                "user_appeared" : "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal"
+        }
+}
+{
+        "company_drives" : {
+                "name" : "IBM",
+                "user_appeared" : "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen"
+        }
+}
+{
+        "company_drives" : {
+                "name" : "Myntra",
+                "user_appeared" : "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja"
+        }
+}
+{
+        "company_drives" : {
+                "name" : "Instagram",
+                "user_appeared" : "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar"
+        }
+}
+{
+        "company_drives" : {
+                "name" : "Amazon",
+                "user_appeared" : "Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag"
+        }
+}
+{
+        "company_drives" : {
+                "name" : "Microsoft",
+                "user_appeared" : "Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal"
+        }
+}
+{
+        "company_drives" : {
+                "name" : "Citrus",
+                "user_appeared" : "Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Ramkrishna Pal , Jessica Menon"
+        }
+}
+{
+        "company_drives" : {
+                "name" : "Amdocs",
+                "user_appeared" : "Tamal Saha , Tanu Roy , Jyoti Maity , Sandarva Podder , Ramkrishna Pal , Animesh Singh"
+        }
+}
+{
+        "company_drives" : {
+                "name" : "Facebook",
+                "user_appeared" : "Rahul Patel , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh"
+        }
+}
+{
+        "company_drives" : {
+                "name" : "Twitter",
+                "user_appeared" : "Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity"
+        }
+}
+4. Find the number of problems solved by the user in codekata
+ANS- db.zenofoctober.find({},{_id:0,users:1,codekata:1}).pretty();
+OUTPUT-
+{
+    "users" : "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh",
+    "codekata" : "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"
+}
+{
+    "users" : "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity",
+    "codekata" : "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70"
+}
+{
+    "users" : "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder",
+    "codekata" : "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57"
+}
+{
+    "users" : "Rahul Patel , Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh",
+    "codekata" : "65 , 12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"
+}
+{
+    "users" : "Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh",
+    "codekata" : "50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"
+}
+{
+    "users" : "Alok Tagore , Kishor Bhadra , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh",
+    "codekata" : "12 , 45 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"
+}
+{
+    "users" : "Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh",
+    "codekata" : "15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"
+}
+{
+    "users" : "Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh",
+    "codekata" : "51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"
+}
+{
+    "users" : "Rahul Patel , Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh",
+    "codekata" : "65 , 55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"
+}
+{
+    "users" : "Karim Khan , Krittika Podder , Arnab Chowdhury , Harshita Sen , Ram Rajan , Shymal Gayen , Kartik Ahuja , Tamal Saha , Tanu Roy , Jyoti Maity , Debarati Sikdar , Shuvojit Bag , Sandarva Podder , Ramkrishna Pal , Jessica Menon , Arup Maji , Animesh Singh",
+    "codekata" : "55 , 50 , 100 , 45 , 80 , 15 , 21 , 51 , 65 , 70 , 85 , 75 , 57 , 66 , 70 , 110 , 40"
+}
+5. Find all the mentors with who has the mentee's count more than 15
+ANS- db.zenofoctober.find({"mentors.mentee":{$gt:15}},{_id:0,mentors:1}).sort({"mentors.mentee":1}).pretty();
+OUTPUT-
+{ "mentors" : { "name" : "Gargi Roy", "mentee" : 16 } }
+{ "mentors" : { "name" : "Gopal Anand", "mentee" : 16 } }
+{ "mentors" : { "name" : "Shyam Prasad", "mentee" : 17 } }
+{ "mentors" : { "name" : "Rajendra Prasad", "mentee" : 18 } }
+{ "mentors" : { "name" : "Raghav Kumar", "mentee" : 19 } }
+{ "mentors" : { "name" : "Kartik Saha", "mentee" : 20 } }
+{ "mentors" : { "name" : "Ramesh Bhardwaj", "mentee" : 20 } }
+
+6. Find the number of users who are absent and task is not submitted  between 15 oct-2020 and 31-oct-2020
+ANS- db.zenofoctober.find({"attendance.date":{$gte:15, $lte:31}},{_id:0,attendance:{absent:1,date:1},tasks:{notsubmit:1}});
+OUTPUT-
+{ "attendance" : { "absent" : 3, "date" : 15 }, "tasks" : { "notsubmit" : 2 } }
+{ "attendance" : { "absent" : 2, "date" : 16 }, "tasks" : { "notsubmit" : 1 } }
+{ "attendance" : { "absent" : 2, "date" : 17 }, "tasks" : { "notsubmit" : 2 } }
+{ "attendance" : { "absent" : 2, "date" : 18 }, "tasks" : { "notsubmit" : 2 } }
+{ "attendance" : { "absent" : 2, "date" : 29 }, "tasks" : { "notsubmit" : 0 } }
+{ "attendance" : { "absent" : 2, "date" : 30 }, "tasks" : { "notsubmit" : 5 } }
